23bcbabdc08afc0d0265b6eeba194ef5d853eec8
Ramzes-Epp <ramzes-epp@rambler.ru>
2022-08-14 18:16:54 +0300

    В вики описал методы контроллера, их маршруты, входящие и исходящие параметры в а так же коды ответов - выводиться все в сваггер

--- MastersAggregatorService/Controllers/OrderController.cs
+++ MastersAggregatorService/Controllers/OrderController.cs
@@ -23,32 +23,37 @@ public class OrderController : BaseController<Order>
     /// GET all order
     /// </summary> 
     /// <returns>List of all Order</returns>  
+    /// <response code="200"> Returns List of all Order.</response>
     [HttpGet]
     public ActionResult GetAll()
     {
         return Ok(_orderRepository.GetAll());
     }
-
+ 
     /// <summary>
     /// GET by Id order
     /// </summary>
-    /// <param Id Order ="id"></param>
+    /// <param name="id"> Id Order</param>
     /// <returns>Order by id</returns>  
+    /// <response code="200"> Returns Order.</response>
+    /// <response code="400"> Order id does not exist.</response>
     [HttpGet("{id}")]
     public ActionResult GetById(int id)
     {
         if (_orderRepository.GetById(id) == null)
-            return new JsonResult("order does not exist");
+            return NotFound($"id Order {id} does not exist.");
 
         return Ok(_orderRepository.GetById(id));
     }
 
     /// <summary>
-    /// Saves a new object 
+    /// Add new Order. 
     /// </summary>
-    /// <param name="idUser">Id Model User</param>
-    /// <param name="idImage">Id Model Image</param>
-    /// <returns>New object with database Id</returns>
+    /// <param name="idUser">Id Model User.</param>
+    /// <param name="arrIdImage">Array Id Model Image.</param>
+    /// <returns>New object with database Id.</returns>
+    /// <response code="200"> Returns int id create Order.</response>
+    /// <response code="400"> idUser id does not exist and can't create order.</response>
     [HttpPost]
     public ActionResult Save(int idUser, int[] arrIdImage)
     {
@@ -63,8 +68,10 @@ public class OrderController : BaseController<Order>
     /// <summary>
     /// delete id order
     /// </summary> 
-    /// <param Id Order ="idOrder"></param>
+    /// <param name = "idOrder"> Id Order </param>
     /// <returns>Delete Id Order</returns>  
+    /// <response code="200"> successfully deleted Order.</response>
+    /// <response code="400"> failed to delete order, such id order does not exist.</response>
     [HttpDelete]
     public ActionResult Delete(int idOrder)
     {
--- MastersAggregatorService/Program.cs
+++ MastersAggregatorService/Program.cs
@@ -1,5 +1,7 @@
 using MastersAggregatorService.Repositories;
 using MastersAggregatorService.Models;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "PrintFramer API",
+        Description = "Calculates the cost of a picture frame based on its dimensions.",
+        TermsOfService = new Uri("https://go.microsoft.com/fwlink/?LinkID=206977"),
+        Contact = new OpenApiContact
+        {
+            Name = "Your name",
+            Email = string.Empty,
+            Url = new Uri("https://www.microsoft.com/learn")
+        }
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 //Добавили в сервис наши Repository 
 builder.Services.AddScoped<ImageRepository>()
