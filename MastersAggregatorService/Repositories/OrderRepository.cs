379f4ee68522315c5e974c2804bff9970c39ec4d
Ramzes-Epp <ramzes-epp@rambler.ru>
2022-09-04 13:53:49 +0300

    Исправления 3.0 переписан OrderRepository т.к изменил таблицу master_shema.images в БД (добавлен столбец, значение по умолчанию null)

--- MastersAggregatorService/Repositories/OrderRepository.cs
+++ MastersAggregatorService/Repositories/OrderRepository.cs
@@ -18,45 +18,44 @@ public class OrderRepository : BaseRepository<Order>, IOrderRepository
     /// <returns></returns>
     public async Task<IEnumerable<Order>> GetAllAsync()
     {
-        var orders = new List<Order>();//список всех Order
-
-        const string sqlQuerySender = @"SELECT DISTINCT orders.id AS ordersid, users.id AS usersid, users.name AS name, users.first_name, users.pfone " +
-                                            @"FROM master_shema.orders " +
-                                            @"INNER JOIN master_shema.users ON users.id = orders.user_id " +
-                                            @"INNER JOIN master_shema.image_of_orders ON image_of_orders.order_id = orders.id " +
-                                            @"ORDER BY ordersid";
-
-        const string sqlQueryImage = @"SELECT orders.id AS ordersid, images.url, images.description, images.id AS imagesid " +
-                                            @"FROM master_shema.orders " +
-                                            @"INNER JOIN master_shema.image_of_orders ON order_id = orders.id " +
-                                            @"INNER JOIN master_shema.images ON images.id = image_of_orders.image_id " +
-                                            @"ORDER BY orders.id";
-
+        var orders = new List<Order>();//список всех Order 
+        const string sqlQueryUserSenderImage = @"SELECT orders.id AS ordersid, users.id AS usersid, users.name AS name, users.first_name, users.pfone, images.url, images.description, images.id AS imagesid " +
+                                                        @"FROM master_shema.orders " +
+                                                        @"LEFT JOIN master_shema.users ON users.id = orders.user_id " +
+                                                        @"LEFT JOIN master_shema.images ON images.order_id = orders.id " +
+                                                        @"ORDER BY orders.id";
+         
         await using var connection = new NpgsqlConnection(ConnectionString);
         connection.Open();
-         
-        IEnumerable<dynamic> resultDynamicQuerySenders = connection.Query(sqlQuerySender);
-        IEnumerable<dynamic> resultDynamicQueryImages = connection.Query(sqlQueryImage);
 
-        foreach (var ObjSender in resultDynamicQuerySenders)
+        IEnumerable<dynamic> resultDynamicQueryUserSenderImage = connection.Query(sqlQueryUserSenderImage);
+
+        var orderGroups = resultDynamicQueryUserSenderImage.GroupBy(r => r.ordersid);//группируем по id order
+
+        foreach (var objOrder in orderGroups) 
         {
-            //собираем обьект Images с resultDynamicQueryImages
+            //собираем обьект Images с orderGroups если есть Image добавляем в коллекцию
             var Images = new List<Image>();
-            foreach (var ObjImages in resultDynamicQueryImages)  //TODO Заменить сборку Images на метод из ImageRepository
+            if (objOrder.First().imagesid != null)
             {
-                if (ObjSender.ordersid == ObjImages.ordersid)
-                    Images.Add(new Image { Id = ObjImages.imagesid, ImageDescription = ObjImages.description, ImageUrl = ObjImages.url });
+                foreach (var objImages in objOrder)  
+                { 
+                    Images.Add(new Image { Id = objImages.imagesid, ImageDescription = objImages.description, ImageUrl = objImages.url });
+                }
             }
-
-            //Собираем обьект Order с resultDynamicQuerySenders
-            Order order = new Order { Id = ObjSender.ordersid, 
-                 Sender = new User { Id = ObjSender.usersid, Name = ObjSender.name, FirstName = ObjSender.first_name, Pfone = ObjSender.pfone }, 
-                 Images = Images };
-
-            orders.Add(order);
-        }
             
-      return orders;
+            //Собираем обьект Order с orderGroups
+            Order newOrder = new Order
+            {
+                Id = objOrder.First().ordersid,
+                Sender = new User { Id = objOrder.First().usersid, Name = objOrder.First().name, FirstName = objOrder.First().first_name, Pfone = objOrder.First().pfone },
+                Images = Images
+            };
+             
+            orders.Add(newOrder);
+        } 
+
+        return orders;
     }
 
     /// <summary>
@@ -76,44 +75,43 @@ public class OrderRepository : BaseRepository<Order>, IOrderRepository
     /// <returns></returns> 
     public async Task<Order> GetByIdAsync(int idOrder)
     {
-        const string sqlQueryUserSender = @"SELECT DISTINCT orders.id AS ordersid, users.id AS usersid, users.name AS name, users.first_name, users.pfone " +
-                                                @"FROM master_shema.orders " +
-                                                @"INNER JOIN master_shema.users ON users.id = orders.user_id " +
-                                                @"INNER JOIN master_shema.image_of_orders ON image_of_orders.order_id = orders.id " +
-                                                @"WHERE orders.id = @idOrder";
-
-        const string sqlQueryImage = @"SELECT orders.id AS ordersid, images.url, images.description, images.id AS imagesid " +
-                                            @"FROM master_shema.orders " +
-                                            @"INNER JOIN master_shema.image_of_orders ON order_id = orders.id " +
-                                            @"INNER JOIN master_shema.images ON images.id = image_of_orders.image_id " +
-                                            @"WHERE orders.id = @idOrder";
-
-
+        const string sqlQueryUserSenderImage = @"SELECT orders.id AS ordersid, users.id AS usersid, users.name AS name, users.first_name, users.pfone, images.url, images.description, images.id AS imagesid " +
+                                                        @"FROM master_shema.orders " +
+                                                        @"LEFT JOIN master_shema.users ON users.id = orders.user_id " +
+                                                        @"LEFT JOIN master_shema.images ON images.order_id = orders.id " +
+                                                        @"WHERE orders.id = @idOrder";
+         
         await using var connection = new NpgsqlConnection(ConnectionString);
         connection.Open();
            
-        IEnumerable<dynamic> resultDynamicQuerySenders = connection.Query(sqlQueryUserSender, new { idOrder });
-        var ObjSender = resultDynamicQuerySenders.First();
+        IEnumerable<dynamic> resultDynamicQueryUserSenderImage = connection.Query(sqlQueryUserSenderImage, new { idOrder });
+         
+        var orderGroup = resultDynamicQueryUserSenderImage.GroupBy(r => r.ordersid);//группируем по id order
 
-        IEnumerable<dynamic> resultDynamicQueryImages = connection.Query(sqlQueryImage, new { idOrder });
+        foreach (var objOrder in orderGroup)
+        {
+            //собираем обьект Images с orderGroups если есть Image добавляем в коллекцию
+            var Images = new List<Image>();
+            if (objOrder.First().imagesid != null)
+            {
+                foreach (var objImages in objOrder)
+                {
+                    Images.Add(new Image { Id = objImages.imagesid, ImageDescription = objImages.description, ImageUrl = objImages.url });
+                }
+            }
 
-        //собираем обьект Images с resultDynamicQueryImages
-        var Images = new List<Image>();
-        foreach (var ObjImages in resultDynamicQueryImages)
-        { 
-            Images.Add(new Image { Id = ObjImages.imagesid, ImageDescription = ObjImages.description, ImageUrl = ObjImages.url });
+            //Собираем обьект Order с orderGroups
+            Order newOrder = new Order
+            {
+                Id = objOrder.First().ordersid,
+                Sender = new User { Id = objOrder.First().usersid, Name = objOrder.First().name, FirstName = objOrder.First().first_name, Pfone = objOrder.First().pfone },
+                Images = Images
+            };
+
+            return newOrder;
         }
 
-        //Собираем обьект Order с resultDynamicQuerySenders
-        Order order = new Order
-        {
-            Id = ObjSender.ordersid,
-            Sender = new User { Id = ObjSender.usersid, Name = ObjSender.name, FirstName = ObjSender.first_name, Pfone = ObjSender.pfone },
-            Images = Images
-        };
-             
-        return order;
-       
+        return null; 
     }
 
     /// <summary>
@@ -137,18 +135,16 @@ public class OrderRepository : BaseRepository<Order>, IOrderRepository
         //SQL запрос - добавляем в таблицу orders новый ордер если user_id существует
         string sqlQueryAddOrders = @"INSERT INTO master_shema.orders (user_id) " +
                                           @"SELECT users.id FROM master_shema.users " +
-                                          @"WHERE master_shema.users.id = @userId " +
+                                          @"WHERE master_shema.users.id = @Id " +
                                           @"RETURNING id";
-        int userId = model.Sender.Id;
 
-        //SQL запрос - добавляем в таблицу image_of_orders новый image если они существуют в таблице 
-        string sqlQueryAddImages = @"INSERT INTO master_shema.image_of_orders (order_id, image_id) " +
-                                          @"VALUES (@resIdNewOrder, (SELECT master_shema.images.id  FROM master_shema.images WHERE master_shema.images.id = @Id)) ";
+        //SQL запрос - изменяем в таблице images номер order_id если существуют в таблице эта image и ее order_id IS NULL
+        string sqlQueryAddImages = @"UPDATE master_shema.images  SET order_id = @resIdNewOrder  WHERE images.id = @Id AND order_id IS NULL";
          
         await using var connection = new NpgsqlConnection(ConnectionString);
         connection.Open();
          
-        int? resIdNewOrder = await connection.QuerySingleAsync<int>(sqlQueryAddOrders, new { userId });
+        int? resIdNewOrder = await connection.QuerySingleAsync<int>(sqlQueryAddOrders, new { model.Sender.Id });
         if (resIdNewOrder == null)
             return null;
 
@@ -184,9 +180,11 @@ public class OrderRepository : BaseRepository<Order>, IOrderRepository
     /// <param name="model"></param>
     /// <returns></returns>
     public async Task DeleteAsync(Order model)
-    { 
-        string sqlQuery = @"DELETE FROM master_shema.image_of_orders WHERE image_of_orders.order_id = @Id; " +
+    {
+        //В таблице master_shema.images в столбце order_id пишем NULL таким оброзом освобождаем картинку для добавления в другой ордер
+        string sqlQuery = @"UPDATE master_shema.images SET order_id = NULL  WHERE images.order_id = @Id; " +
                           @"DELETE FROM master_shema.orders WHERE orders.id = @Id";
+         
         await using var connection = new NpgsqlConnection(ConnectionString);
         connection.Open();
 
