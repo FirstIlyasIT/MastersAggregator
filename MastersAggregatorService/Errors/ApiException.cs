53dc8f864d207e5cb74b13975b981a1a00ee6812
youngsunsong <inhaltua@gmail.com>
2022-09-13 00:36:11 +0300

    Добавил метод для записи в БД

--- MastersAggregatorService/Errors/ApiException.cs
+++ MastersAggregatorService/Errors/ApiException.cs
@@ -2,13 +2,16 @@
 
 public class ApiException
 {
-    public ApiException(int statusCode, string message = null, string details = null)
+    public ApiException(int statusCode, string userName = null, string message = null, string details = null)
     {
+        
         StatusCode = statusCode;
+        UserName = userName;
         Message = message;
         Details = details;
     }
 
+    public string UserName { get; set; }
     public int StatusCode { get; set; }
     public string Message { get; set; }
     public string Details { get; set; }
--- MastersAggregatorService/Middleware/ExceptionMiddleware.cs
+++ MastersAggregatorService/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net;
 using System.Text.Json;
+using Dapper;
 using MastersAggregatorService.Errors;
+using Npgsql;
 
 namespace MastersAggregatorService.Middleware;
 
@@ -9,13 +11,15 @@ public class ExceptionMiddleware
     private readonly RequestDelegate _next;
     private readonly ILogger<ExceptionMiddleware> _logger;
     private readonly IHostEnvironment _env;
+    private readonly IConfiguration _configuration;
 
 
-    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env, IConfiguration configuration)
     {
         _next = next;
         _logger = logger;
         _env = env;
+        _configuration = configuration;
     }
 
     public async Task InvokeAsync(HttpContext context)
@@ -33,16 +37,32 @@ public class ExceptionMiddleware
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             var response = _env.IsDevelopment()
-                ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-                : new ApiException(context.Response.StatusCode, "Internal Server Error");
+                ? new ApiException(context.Response.StatusCode, context.User.ToString(), ex.Message, ex.StackTrace?.ToString())
+                : new ApiException(context.Response.StatusCode, context.User.ToString(), "Internal Server Error");
 
             var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
             var json = JsonSerializer.Serialize(response, options);
 
+            await SaveExceptionToDbAsync(new ApiException(context.Response.StatusCode, context.User.ToString(),
+                ex.Message, ex.StackTrace?.ToString()));
+
             await context.Response.WriteAsync(json);
+            
 
             throw;
         }
+    }
+
+    public async Task SaveExceptionToDbAsync(ApiException ex)
+    {
+        const string sqlQuery = 
+            @"INSERT INTO master_shema.errors (status_code, user_name, message, details)" +
+            $@"VALUES (@{nameof(ex.StatusCode)}), (@{nameof(ex.UserName)}), (@{nameof(ex.Message)}), (@{nameof(ex.Details)})";
+
+        await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("ConnectionString"));
+        connection.Open();
+
+        await connection.ExecuteAsync(sqlQuery);
     }
 }
