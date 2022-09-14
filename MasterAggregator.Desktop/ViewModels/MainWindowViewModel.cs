09aeadfa917f6f2f6f744890d7707fa0675e9c34
Ilyas <neugolok@outlook.com>
2022-09-14 12:34:21 +0500

    Собрал десктопный проект

---
+++ MasterAggregator.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MasterAggregator.Desktop.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome master!";
+    }
+}
---
+++ MasterAggregator.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace MasterAggregator.Desktop.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
